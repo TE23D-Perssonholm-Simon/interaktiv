cb526989be7549f76ef687934bb4adff1ebdf36f
Simon Perssonholm <Simon.perssonholm@elev.ga.ntig.se>
2024-08-30 10:52:33 +0200

    started

--- interaktiv/Program.cs
+++ interaktiv/Program.cs
@@ -1,1 +1,39 @@
-﻿System.Console.WriteLine("hej");
+﻿using System.Runtime.InteropServices.Marshalling;
+
+string decision;
+System.Console.WriteLine(@"You are Alex, a seasoned explorer known for your bravery and intelligence. 
+
+You've just received a mysterious map that supposedly leads to a hidden treasure in the heart of the 
+Forbidden Jungle. 
+
+Determined to find it, you set off on your adventure.");
+
+Console.ReadLine();
+
+Console.Clear();
+System.Console.WriteLine(@"As you arrive at the edge of the Forbidden Jungle, you find two paths diverging. 
+
+The left path looks well-trodden but dark and ominous, while the right path is overgrown and filled with dense foliage.
+
+A:left
+
+B:right");
+
+decision = Console.ReadLine();
+
+if (decision == "left")
+{
+
+}
+else if (decision == "right")
+{
+
+}
+else
+{
+    
+}
+
+
+
+
